2266f668f76402eadb3a63caebf419e3d91b3a82
Isaac Gonzalez <84966379+IsaacAGonzalez@users.noreply.github.com>
2023-01-04 23:59:29 -0800

    Figure out sequelize modeling

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,6 @@
 // import important parts of sequelize library
+const { DECIMAL } = require('sequelize');
+const { NUMERIC } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
@@ -10,6 +12,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        is: DECIMAL,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        is: NUMERIC,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
